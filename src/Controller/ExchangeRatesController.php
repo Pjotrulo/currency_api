5cdbabd33acbe6ac38f266913aa3154c23beee8e
Piotr Godwin <99688080+Pjotrulo@users.noreply.github.com>
2023-05-07 17:33:34 +0200

    Added POST method - add currency

--- src/Controller/ExchangeRatesController.php
+++ src/Controller/ExchangeRatesController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\ExchangeRates;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api', name: 'api_')]
+
 class ExchangeRatesController extends AbstractController
 {
-    #[Route('/exchange/rates', name: 'app_exchange_rates')]
-    public function index(): JsonResponse
+    #[Route('/add/{currency}/{amount}', name: 'app_exchange_rates')]
+    public function add(ManagerRegistry $manager, string $currency, float $amount): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ExchangeRatesController.php',
-        ]);
+        if($currency == 'GBP' || $currency == 'USD' || $currency == 'EUR') {
+            $entityManager = $manager->getManager();
+
+            $date = new DateTime('now');
+
+            $exchangeRates = new ExchangeRates();
+            $exchangeRates->setCurrency($currency);
+            $exchangeRates->setDate($date);
+            $exchangeRates->setAmount(number_format($amount, 2, '.'));
+
+            $entityManager->persist($exchangeRates);
+            $entityManager->flush();
+
+            return $this->json("Added data successfully!");
+        } else {
+            return $this->json("Currency ".$currency." not supported!! Choose GBP, USD or EUR");
+        }
     }
 }
