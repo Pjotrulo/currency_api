873319349548971779692996546978fec27d7ee2
Piotr Godwin <99688080+Pjotrulo@users.noreply.github.com>
2023-05-08 11:49:09 +0200

    Update ExchangeRatesController.php

--- src/Controller/ExchangeRatesController.php
+++ src/Controller/ExchangeRatesController.php
@@ -13,27 +13,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ExchangeRatesController extends AbstractController
 {
-    #[Route('/add/{currency}/{amount}', name: 'app_exchange_rates')]
+    #[Route('/add/{currency}/{amount}', name: 'app_exchange_rates_add')]
     public function add(ExchangeRatesRepository $repository, string $currency, float $amount): JsonResponse
     {
         if($currency == 'GBP' || $currency == 'USD' || $currency == 'EUR') {
             
             $date = new DateTime('now');
 
+            $properties = $repository->findByCurrencyAndDate($currency, date_format($date, "Y-m-d"));
+
             $exchangeRates = new ExchangeRates();
             $exchangeRates->setCurrency($currency);
             $exchangeRates->setDate($date);
             $exchangeRates->setAmount(number_format($amount, 2, '.'));
 
-            $repository->save($exchangeRates, true);
+            if (!$properties) {
+                $repository->save($exchangeRates, true);
+                return $this->json("Added");
+            } else if ($properties) {
+                return $this->json("This currency is already exist.");
+            }
 
-            return $this->json("Added data successfully!");
         } else {
             return $this->json("Currency ".$currency." not supported!! Choose GBP, USD or EUR");
         }
     }
 
-    #[Route('/getCurrency/{currency}/{date}', name: 'app_exchange_rates')]
+    #[Route('/getCurrency/{currency}/{date}', name: 'app_exchange_rates_get')]
     public function getCurrency(ExchangeRatesRepository $repository, string $currency, string $date): JsonResponse
     {
         if($currency == 'GBP' || $currency == 'USD' || $currency == 'EUR') {
